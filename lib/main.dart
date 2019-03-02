99357433f1e8e3411e68203e72b39b1ae053158b
tresende <thiago.gcresende@gmail.com>
2019-03-01 21:59:13 -0300

    Paddings

--- lib/main.dart
+++ lib/main.dart
@@ -28,47 +28,50 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
           child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(
-            Icons.person_outline,
-            size: 120,
-            color: Colors.green,
-          ),
-          TextField(
-            style: TextStyle(color: Colors.green, fontSize: 25),
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso em KG",
-                labelStyle: TextStyle(color: Colors.green)),
-          ),
-          TextField(
-            style: TextStyle(color: Colors.green, fontSize: 25),
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura em cm",
-                labelStyle: TextStyle(color: Colors.green)),
-          ),
-          Container(
-              height: 50,
-              child: (RaisedButton(
-                  color: Colors.green,
-                  onPressed: () {},
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                  )))),
-          Text(
-            "Info",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25),
-          )
-        ],
-      )),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.person_outline,
+                size: 120,
+                color: Colors.green,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.green, fontSize: 20),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso em KG",
+                    labelStyle: TextStyle(color: Colors.green)),
+              ),
+              TextField(
+                style: TextStyle(color: Colors.green, fontSize: 20),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura em cm",
+                    labelStyle: TextStyle(color: Colors.green)),
+              ),
+              Padding(
+                child: Container(
+                    height: 50,
+                    child: (RaisedButton(
+                        color: Colors.green,
+                        onPressed: () {},
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )))),
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+              ),
+              Text(
+                "Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 20),
+              )
+            ],
+          )),
     );
   }
 }
