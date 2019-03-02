769a595dec17db82f9ee4dc6cb1bf12211378f8d
tresende <thiago.gcresende@gmail.com>
2019-03-01 21:50:33 -0300

    botoes e inputs

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,35 @@ class _HomeState extends State<Home> {
             size: 120,
             color: Colors.green,
           ),
+          TextField(
+            style: TextStyle(color: Colors.green, fontSize: 25),
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso em KG",
+                labelStyle: TextStyle(color: Colors.green)),
+          ),
+          TextField(
+            style: TextStyle(color: Colors.green, fontSize: 25),
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura em cm",
+                labelStyle: TextStyle(color: Colors.green)),
+          ),
+          Container(
+              height: 50,
+              child: (RaisedButton(
+                  color: Colors.green,
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  )))),
+          Text("Info",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.green, fontSize: 25),
+          )
         ],
       ),
     );
